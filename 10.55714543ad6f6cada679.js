fc3f730a5593b2a87f03b30e6b3bd9b28d200738
janlego <janlego@users.noreply.github.com>
2022-03-15 19:03:14 +0000

    deploy: 5a718fd925e127272671750101596803d8c6806e

---
+++ 10.55714543ad6f6cada679.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{KZOR:function(n){n.exports=JSON.parse('[{"title":"spring","sound":"spring.mp3","image":"spring.png"},{"title":"summer","sound":"summer.mp3","image":"summer.png"},{"title":"autumn","sound":"autumn.mp3","image":"autumn.png"},{"title":"winter","sound":"winter.mp3","image":"winter.png"}]')}}]);
